86434910374834bfdd04ab5c3e5f92200dcd02ec
unknown <rocio.llopez@hotmail.com>
2022-02-03 15:12:35 -0300

    form ready

--- assets/js/main.js
+++ assets/js/main.js
@@ -275,12 +275,5 @@
     })
   });
 
-  const $form = document.querySelector("#form")
-      $form.addEventListener("submit",handleSubmit)
-      function handleSubmit(event) {
-        event.preventDefault{}
-        const form = new FormData(this)
-        console.log (form.get("name"))
-      }
 
 })()
