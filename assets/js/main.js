6aa5dae0f7a2ba3095fd203c371f4d98cedea6ad
unknown <rocio.llopez@hotmail.com>
2022-02-03 11:40:28 -0300

    form

--- assets/js/main.js
+++ assets/js/main.js
@@ -275,4 +275,12 @@
     })
   });
 
+  const $form = document.querySelector("#form")
+      $form.addEventListener("submit",handleSubmit)
+      function handleSubmit(event) {
+        event.preventDefault{}
+        const form = new FormData(this)
+        console.log (form.get("name"))
+      }
+
 })()
